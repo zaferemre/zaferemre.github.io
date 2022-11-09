d1b15e5b0825dd5e0bb1360bb6f239df958ef5b9
zaferemre <zafer.calisir@yemeksepeti.com>
2022-11-09 20:26:56 +0300

    Revision 2

--- pages/hakkimizda.js
+++ pages/hakkimizda.js
@@ -1,10 +1,10 @@
 const header = document.querySelector(".header");
-window.onscroll = function() {
-    var top = window.scrollY;
-    console.log(top);
-    if (top >= 50) {
-        header.classList.add("active");
-    } else {
-        header.classList.remove("active");
-    }
+window.onscroll = function () {
+  var top = window.scrollY;
+  console.log(top);
+  if (top >= 10) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
 };
